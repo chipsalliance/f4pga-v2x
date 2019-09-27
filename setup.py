35060b4412a61df4f1a18027ea59c76398a5a5d0
Karol Gugala <kgugala@antmicro.com>
2019-09-27 09:50:26 +0200

    Move code from arch-defs
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="v2x",
+    version="0.0.1",
+    author="SymbiFlow Authors",
+    author_email="symbiflow@lists.librecores.org",
+    description="Python library for generating VPR architecture \
+                description files from Verilog models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SymbiFlow/python-symbiflow-v2x",
+    packages=setuptools.find_packages(),
+    install_requires=['ply', 'pyjson'],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License",
+        "Operating System :: OS Independent",
+    ],
+    #entry_points={
+    #    'console_scripts': ['sdf_timing_parse=sdf_timing.sdfparse:main'],
+    #},
+)
---
+++ v2x/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from .v2x import vlog_to_pbtype
+
+def main():
+    echo("main")
+
+if __name__ = '__main__':
+    main()
