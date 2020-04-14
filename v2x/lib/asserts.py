d2589e6b2f101854edfd10d9494355ca99594a22
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-04-15 00:58:13 +0800

    Add newline to asserts.py according to PEP8 requirements
    Signed-off-by: Daniel Lim Wee Soong <weesoong.lim@gmail.com>

--- v2x/lib/asserts.py
+++ v2x/lib/asserts.py
@@ -14,6 +14,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+
 def __safe_call(f, a):
     """Call a function and capture all exceptions."""
     try:
