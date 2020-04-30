a3268332d8117369be3b97c3b67b25265893b6b0
Maciej Kurc <mkurc@antmicro.com>
2020-04-30 15:25:46 +0200

    Updated test.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- tests/fasm_params/module.sim.v
+++ tests/fasm_params/module.sim.v
@@ -1,11 +1,14 @@
-(* FASM_PARAMS = "FEATURE_INIT=INIT;ENABLE_INV=IS_SOMETHING_INVERTED" *)
 (* whitebox *)
 module MODULE(I1, I2, O);
     input  wire I1;
     input  wire I2;
     output wire O;
 
+    (* FASM *)
     parameter INIT = 0;
+    (* FASM="INVERT" *)
     parameter IS_SOMETHING_INVERTED = 0;
+
+    assign O = I1 | I2;
 
 endmodule
