fcc2975c9825d004ac365fb75c6531a9054e7b46
Maciej Kurc <mkurc@antmicro.com>
2020-09-09 16:17:11 +0200

    Added missing licensing information
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- tests/fasm_features/no_modes/module.sim.v
+++ tests/fasm_features/no_modes/module.sim.v
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2020  The SymbiFlow Authors.
+ *
+ * Use of this source code is governed by a ISC-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/ISC
+ *
+ * SPDX-License-Identifier:     ISC
+ */
+
 (* FASM_FEATURES="IN_USE;ENABLE_FEATURE_X" *)
 (* whitebox *)
 module MODULE(I0, I1, O);
