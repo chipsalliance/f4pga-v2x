ef11264d1e9e095bfbe0ef5c67f04189a50afb86
Maciej Kurc <mkurc@antmicro.com>
2020-09-10 15:50:47 +0200

    Added licensing information
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- tests/clock_mux/gmux.sim.v
+++ tests/clock_mux/gmux.sim.v
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2020  The SymbiFlow Authors.
+ *
+ * Use of this source code is governed by a ISC-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/ISC
+ *
+ * SPDX-License-Identifier:     ISC
+ */
+
 // A model of a clock multiplexer with two clock inputs, one clock output and
 // a select input.
 
